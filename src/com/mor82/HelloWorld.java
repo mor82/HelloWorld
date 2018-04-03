51762c1db8e08943c24d6054d29ae32d1ae1663c
mor82 <mor82@gmx.com>
2018-04-03 13:19:45 +0300

    Переименован метод

--- src/com/mor82/HelloWorld.java
+++ src/com/mor82/HelloWorld.java
@@ -1,7 +1,10 @@
 package com.mor82;
 
-public class Main {
+public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.print("Hello World");
+
+
     }
+
 }
